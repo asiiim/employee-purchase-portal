203de1df2be5f2aa2e898016b430d9825bd3c152
Aashim Bajracharya <ashimbazracharya@gmail.com>
2021-03-19 23:05:34 +0545

    [ADD] Add Docker Ready for Odoo Addons

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM odoo:14.0
+LABEL Aashim Bajracharya. <ashimbazracharya@gmail.com>
+
+USER root
+RUN mkdir -p /mnt/custom_addons
+
+RUN apt-get update \
+    && apt-get install
+
+RUN chown -R odoo /mnt
+RUN chown -R odoo /mnt/*
---
+++ addons/employee_purchase_portal/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Employee Purchase Portal',
+    'version' : '14.0.0.1',
+    'summary': 'Purchase portal for the employee and the manager.',
+    'sequence': 1,
+    'description': """
+        Employee Purchase Portal
+        ====================
+        This module builds the portal for the employee and the manager to fulfill the worflow
+        of the purchase process. Moreover, there are internal users who actually process the 
+        purchase if verified by the manager through the portal.
+    """,
+    'category': 'Purchase',
+    'author': 'Aashim Bajracharya',
+    'email': 'ashimbazracharya@gmail.com',
+    'depends' : ['purchase', 'portal'],
+    'data': [
+        # 'security/security.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
