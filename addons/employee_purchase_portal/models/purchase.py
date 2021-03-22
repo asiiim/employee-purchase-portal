dfb32f8334800d2d4f692b1ce1a7f32dd977bfbd
Aashim Bajracharya <ashimbazracharya@gmail.com>
2021-03-22 09:31:20 +0545

    [ADD] Complete todo's
    - Make select only allowed categ products
    - Add action to approve and reject rfq
    - Restrict to create rfq if the last rejected
    rfq is not a month ago.

--- addons/employee_purchase_portal/models/purchase.py
+++ addons/employee_purchase_portal/models/purchase.py
@@ -1,11 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import math
+import logging
+
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
-import logging
 
 _logger = logging.getLogger(__name__)
 
-# -*- coding: utf-8 -*-
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
@@ -22,26 +25,34 @@ class PurchaseOrder(models.Model):
     ], string='Order Portal Status', readonly=True, index=True,
         copy=False, default='draft', tracking=True)
 
-    # Boolean field to store the rejected RFQ
-    rejected_rfq = fields.Boolean("Rejected", copy=False, default=False)
+    purchase_maker = fields.Many2one(related='user_id.partner_id', 
+        string="Purchase Maker",
+        store=True)
 
-    # Domain the vendor list as per the selected product.
-    portal_product_id = fields.Many2one('product.product', string='Product', 
-        domain=[('purchase_ok', '=', True)], change_default=True)
+    allowed_product_categories = fields.Many2many(related="purchase_maker.allowed_product_categories")
+
+    portal_product_id = fields.Many2one('product.product', 
+        string='Product', 
+        domain="[('purchase_ok', '=', True), ('categ_id', 'in', allowed_product_categories)]", 
+        change_default=True)
+
     related_product_template = fields.Many2one(related='portal_product_id.product_tmpl_id', 
         string='Product Template', store=True)
+    
     supplier_info = fields.Many2one('product.supplierinfo', string='Available Vendor', 
         change_default=True, 
         tracking=True,
         domain="[('product_tmpl_id', '=', related_product_template)]")
+
 
     # For now let's assign the vendor field with on_change of supplier_info
     # Need to called the model to set the value from the portal later.
     @api.onchange('supplier_info')
     def on_change_supplier(self):
         self.partner_id = self.supplier_info.name
+
     
-    # TODO: Override the method "action_convert_to_order"
+    # Override the method "action_convert_to_order"
     def action_convert_to_order(self):
         """ 
             - Check if the user group is "Accounting Team".
@@ -49,27 +60,63 @@ class PurchaseOrder(models.Model):
                 the portal_state to "Purchase In Progress".
             - Check the group of the current user.
         """
-        accounting_team_group_ref = \
-            self.env.ref('employee_purchase_portal.group_accounting_team')
+        # TODO: Create purchase order line
 
-        if self.env.user.has_group(ccounting_team_group_ref):
+        if self.env.user.has_group('employee_purchase_portal.group_accounting_team'):
 
             result = super(PurchaseOrder, self).action_convert_to_order()
+        else:
+            raise UserError("You are not allowed to make Purchase Order.\nPlease consult the Accounting Team.")
 
-    # TODO: Method to Approve or Reject RFQ by Manager Group Users
-    """
-        - Make sure this method is accessible to ony Manager group
-        - Make sure the portal_state of the RFQ is "Draft"
-        - Change the state to "Approve" or "Reject".
-        - Set the field "rejected_rfq" to True.
-    """
 
-    # TODO: Override the create() method.
-    """
-        - Check if the last rejected rfq is older than 30 days.
-        - Check that users of the group "Manager" & "Acc Team" cannot create RFQ
-    """
+    # Method to Approve or Reject RFQ by Manager Group Users
+    def button_approve(self):
+        """
+            - Make sure this method is accessible to ony Manager group
+            - Make sure the portal_state of the RFQ is "Draft"
+            - Change the state to "Approve" or "Reject".
+        """
+        if self.env.user.has_group('employee_purchase_portal.group_purchase_manager') \
+            and self.portal_state == "draft":
+            self.write({'portal_state': 'approved'})
+        else:
+            raise UserError("You don't have access to approve the RFQ.")
 
-    # TODO: Make domain to view only self list of order
+
+    # Override cancel action to change the portal state  to "rejected".
+    def button_cancel(self):
+        if self.env.user.has_group('employee_purchase_portal.group_purchase_manager'):
+            result = super(PurchaseOrder, self).button_cancel()
+            self.write({
+                'portal_state': 'rejected'
+            })
+        else:
+            raise UserError("You don't have access to reject the RFQ.")
+
+    
+    # Override the create() method.
+    @api.model
+    def create(self, vals):
+        """
+            - Check if the last rejected rfq is older than 30 days.
+        """
+        orders = self.env['purchase.order'].search([('user_id', '=', self.env.user.id)])
+        if orders:
+            for order in orders:
+                if order.portal_state == 'rejected':
+                    
+                    order_date = fields.Datetime.from_string(order.date_order)
+                    today = fields.Datetime.now()
+                    time_delta = today - order_date
+                    
+                    # No. of day since last rejection from now.
+                    rejected_days = math.ceil(time_delta.days + float(time_delta.seconds) / 86400) + 1.0
+                    
+                    if  rejected_days < 30:
+                        raise UserError('Sorry, your last rejected order is still less than a month.')
+        return super().create(vals)
+    
+
+    # TODO: Make domain to view only self list of order by the employee
 
 
